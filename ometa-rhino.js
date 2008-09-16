b94c37aba5727fc0ef51cda14b01c43f6724bf0e
Alessandro Warth <alexwarth@gmail.com>
2008-09-16 21:52:01 +0000

    updated ometa-rhino.js to work w/ modified v8 shell

--- ometa-rhino.js
+++ ometa-rhino.js
@@ -6,11 +6,13 @@ load("bs-ometa-compiler.js")
 load("bs-ometa-optimizer.js")
 load("bs-ometa-js-compiler.js")
 
-function translateCode(s) {
+alert = print
+
+translateCode = function(s) {
   var translationError = function(m, i) { alert("Translation error - please tell Alex about this!"); throw fail },
       tree             = BSOMetaJSParser.matchAll(s, "topLevel", undefined, function(m, i) { throw fail.delegated({errorPos: i}) })
   return BSOMetaJSTranslator.match(tree, "trans", undefined, translationError)
 }
 
-function ometa(s) { return eval(translateCode(s)) }
+ometa = function(s) { return eval(translateCode(s)) }
 

173a79152e7277eb81c5a17069670f062b551930
Alessandro Warth <alexwarth@gmail.com>
2010-01-21 22:19:18 +0000

    re-generated all of the .js files from the original ometa code (which should make the ometa/js parser, codegen, etc. a bit faster)

--- bs-project-list-parser.js
+++ bs-project-list-parser.js
@@ -1,1 +1,3 @@
-ProjectListParser=BSJSParser.delegated();ProjectListParser['proj']=function() {var $elf=this;return (function(){$elf._apply("spaces");return $elf._apply("iName")})()};ProjectListParser['projs']=function() {var $elf=this,x,xs;return (function(){x=$elf._apply("proj");xs=$elf._many(function(){return (function(){$elf._apply("spaces");$elf._applyWithArgs("exactly", ",");return $elf._apply("proj")})()});return [x].concat(xs)})()};ProjectListParser.prototype=ProjectListParser;
+ProjectListParser=BSJSParser.delegated({
+"proj":function(){var $elf=this;return (function(){$elf._apply("spaces");return $elf._apply("iName")})()},
+"projs":function(){var $elf=this,x,xs;return (function(){x=$elf._apply("proj");xs=$elf._many(function(){return (function(){$elf._apply("spaces");$elf._applyWithArgs("exactly",",");return $elf._apply("proj")})()});return [x].concat(xs)})()}})

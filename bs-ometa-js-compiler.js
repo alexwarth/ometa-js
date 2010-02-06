bd9fe9bedfc2bffc42a270028e038a19579e0ce6
Alessandro Warth <alexwarth@gmail.com>
2010-02-06 03:12:23 +0000

    finished bootstrapping based on changes from this afternoon, and enhanced ''curly semantic actions'' so that the last srcElement doesn't have to be an expression

--- bs-ometa-js-compiler.js
+++ bs-ometa-js-compiler.js
@@ -1,3 +1,3 @@
 {BSOMetaJSParser=BSJSParser.delegated({
-"srcElem":function(){var $elf=this,r;return this._or((function(){return (function(){this._apply("spaces");r=this._applyWithArgs("foreign",BSOMetaParser,'grammar');this._apply("sc");return r}).call(this)}),(function(){return BSJSParser._superApplyWithArgs(this,'srcElem')}))}});BSOMetaJSTranslator=BSJSTranslator.delegated({
-"Grammar":function(){var $elf=this;return this._applyWithArgs("foreign",BSOMetaTranslator,'Grammar')}})}
+"srcElem":function(){var $elf=this,_fromIdx=this.input.idx,r;return this._or((function(){return (function(){this._apply("spaces");r=this._applyWithArgs("foreign",BSOMetaParser,'grammar');this._apply("sc");return r}).call(this)}),(function(){return BSJSParser._superApplyWithArgs(this,'srcElem')}))}});BSOMetaJSTranslator=BSJSTranslator.delegated({
+"Grammar":function(){var $elf=this,_fromIdx=this.input.idx;return this._applyWithArgs("foreign",BSOMetaTranslator,'Grammar')}})}
